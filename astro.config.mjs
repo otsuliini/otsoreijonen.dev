4051cbfc1677a50447ac02023837b556bdf8fb55
boom1401 <otso.reijonen@icloud.com>
2026-04-25 02:11:04 +0300

    Added some initial files to define the structure better.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+
+    site: 'otsoreijonen.dev',
+    base: '/', 
+    trailingSlash: "ignore"
+
+});
